792e541cc972c048f5dbab4f7295f46c4d7379e7
yangdole\yandole <whxodid666@naver.com>
2025-06-05 11:27:03 +0900

    # test-server # cicd-server vagrant 수정

--- test-server/main.go
+++ test-server/main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CORSM() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization, Origin")
+		c.Header("Access-Control-Allow-Origin", "http://localhost:5173")
+		c.Header("Access-Control-Allow-Method", "GET, DELETE, POST")
+		c.Header("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+	}
+}
 
 func main() {
-	fmt.Println("Hello")
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+
+	r.Use(CORSM())
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/start", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "ok",
+		})
+	})
+
+	r.GET("/live", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "ok",
+		})
+	})
+
+	r.GET("/read", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "ok",
+		})
+	})
+
+	r.Run()
 }
